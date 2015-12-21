5901df7bc722d0991420574507f0d8391486e6e3
Madhav Puri <madhav.puri@gmail.com>
2015-12-21 05:26:26 -0800

    add more utilities for working with systemd units
    Signed-off-by: Madhav Puri <madhav.puri@gmail.com>

--- system.go
+++ system.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"fmt"
+	"strings"
 	"time"
 
 	log "github.com/Sirupsen/logrus"
@@ -67,4 +68,89 @@ func StartEtcd(nodes []vagrantssh.TestbedNode) error {
 	}
 
 	return nil
+}
+
+//ServiceStart starts a systemd service unit
+func ServiceStart(n vagrantssh.TestbedNode, srv string) (string, error) {
+	return n.RunCommandWithOutput(fmt.Sprintf("sudo systemctl start %s", srv))
+}
+
+//ServiceStatus queries and returns status result of systemd service unit
+func ServiceStatus(n vagrantssh.TestbedNode, srv string) (string, error) {
+	return n.RunCommandWithOutput(fmt.Sprintf("systemctl status %s", srv))
+}
+
+// WaitForDone polls for checkDoneFn function to return true up until specified timeout
+func WaitForDone(doneFn func() (string, bool), timeoutSec int, timeoutMsg string) (string, error) {
+	tick := time.Tick(time.Duration(2) * time.Second)
+	timeout := time.Tick(time.Duration(timeoutSec) * time.Second)
+	doneCount := 0
+	for {
+		select {
+		case <-tick:
+			if ctxt, done := doneFn(); done {
+				doneCount++
+				// add some resilliency to poll inorder to avoid false positives,
+				// while polling more frequently
+				if doneCount == 2 {
+					// end poll
+					return ctxt, nil
+				}
+			}
+			//continue polling
+		case <-timeout:
+			ctxt, done := doneFn()
+			if !done {
+				return ctxt, fmt.Errorf("wait timeout. Msg: %s", timeoutMsg)
+			}
+			return ctxt, nil
+		}
+	}
+}
+
+//ServiceStartAndWaitForUp starts a systemd service unit and waits for it to be up
+func ServiceStartAndWaitForUp(n vagrantssh.TestbedNode, srv string, timeoutSec int) (string, error) {
+	out, err := ServiceStart(n, srv)
+	if err != nil {
+		return out, err
+	}
+
+	return WaitForDone(func() (string, bool) {
+		out, err := ServiceStatus(n, srv)
+		if err == nil && strings.Contains(out, "Active: active") {
+			return out, true
+		}
+		return out, false
+	}, timeoutSec, fmt.Sprintf("it seems that service %q is not running", srv))
+}
+
+//ServiceStop stops a systemd service unit
+func ServiceStop(n vagrantssh.TestbedNode, srv string) (string, error) {
+	return n.RunCommandWithOutput(fmt.Sprintf("sudo systemctl stop %s", srv))
+}
+
+//ServiceRestart restarts a systemd service unit
+func ServiceRestart(n vagrantssh.TestbedNode, srv string) (string, error) {
+	return n.RunCommandWithOutput(fmt.Sprintf("sudo systemctl restart %s", srv))
+}
+
+//ServiceRestartAndWaitForUp starts a systemd service unit and waits for it to be up
+func ServiceRestartAndWaitForUp(n vagrantssh.TestbedNode, srv string, timeoutSec int) (string, error) {
+	out, err := ServiceRestart(n, srv)
+	if err != nil {
+		return out, err
+	}
+
+	return WaitForDone(func() (string, bool) {
+		out, err := ServiceStatus(n, srv)
+		if err == nil && strings.Contains(out, "Active: active") {
+			return out, true
+		}
+		return out, false
+	}, timeoutSec, fmt.Sprintf("it seems that service %q is not running", srv))
+}
+
+//ServiceLogs queries and returns last 100 lines from systemd service unit logs
+func ServiceLogs(n vagrantssh.TestbedNode, srv string) (string, error) {
+	return n.RunCommandWithOutput(fmt.Sprintf("sudo systemctl status -ln100 %s", srv))
 }
